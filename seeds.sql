45899b0c21b7505a06c158a87c18139825fc22c2
bulkingfire <118399104+bulkingfire@users.noreply.github.com>
2023-05-24 19:11:23 -0500

    Add files via upload

---
+++ seeds.sql
@@ -0,0 +1,39 @@
+INSERT INTO department (dept_name)
+
+ VALUES ("Engineering"),
+        ("Finance"),
+
+        ("Legal"),
+        ( "Sales");
+
+
+INSERT INTO employee (first_name, last_name, role_id)
+
+VALUES  ("bob", "Ahab", 1),
+
+
+        ("Quee", "Queg", 2),
+        ("Moby", "bat", 3),
+        ("Ish", "Mael", 4),
+
+        ("R.J.", "MacReady", 5),
+        ("vageli", "Chang", 6),
+        ( "Eren", "Yaeger", 7),
+        ("Annie", "yu", 8);
+
+
+
+INSERT INTO roles (job_title, role_dept, salary)
+
+VALUES  ("Sales Lead", "Sales", "110000"),
+        ("Sales Person", "Sales", "90000"),
+
+        ("Lead Engineer", "Engineering", "140000"),
+        ("Software Engineer", "Engineering", "150000"),
+
+        ("Account Manager", "Finance", "180000"),
+        ("Acountant", "Finance", "200000"),
+
+        ("Legal Team Lead", "Legal", "270000"),
+        ( "Lawyer", "Legal", "180000");
+     
